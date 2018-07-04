637b941ec87e89bf9ce721c13b5f9bfbd48eb29c
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2018-07-04 01:33:37 -0500

    Added generators and iterables to the API.

--- phtest.js
+++ phtest.js
@@ -1,11 +1,5 @@
 const Heap = require('./lib/pairing-heap');
 
 const h = new Heap((a, b) => a < b);
-
-[1, 3, 5, 4, 2].forEach(value => h.push(value));
-
-const output = [];
-while (!h.empty) {
-  output.push(h.pop());
-}
-console.log(output);
+h.pushValues([1, 3, 5, 4, 2]);
+console.log([...h]);
