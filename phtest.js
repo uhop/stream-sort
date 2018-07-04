fec42385ae28ef20954bacaf46100e5fe742fbbc
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2018-07-04 01:21:57 -0500

    Switch to lists to economize on cons. Renamed API methods.

--- phtest.js
+++ phtest.js
@@ -2,11 +2,10 @@ const Heap = require('./lib/pairing-heap');
 
 const h = new Heap((a, b) => a < b);
 
-[1, 3, 5, 4, 2].forEach(value => h.insert(value));
+[1, 3, 5, 4, 2].forEach(value => h.push(value));
 
 const output = [];
 while (!h.empty) {
-  output.push(h.min);
-  h.deleteMin();
+  output.push(h.pop());
 }
 console.log(output);
