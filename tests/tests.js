d56d1d90e5ea0bae77bfb4c93f807b38fb3755e8
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2018-07-04 19:45:52 -0500

    Rearrangement, removed obsolete files, new tools.

--- tests/tests.js
+++ tests/tests.js
@@ -1,16 +1,7 @@
 'use strict';
 
-var unit = require('heya-unit');
+const unit = require('heya-unit');
 
-require('./test_classic.js');
-require('./test_parser.js');
-require('./test_streamer.js');
-require('./test_packer.js');
-require('./test_filter.js');
-require('./test_escaped.js');
-require('./test_source.js');
-require('./test_emitter.js');
-require('./test_assembler.js');
-require('./test_array.js');
+// require('./test_array.js');
 
 unit.run();
