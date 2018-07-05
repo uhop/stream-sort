963aac04a67c05a2e58d3930dbb0b531f0f6585f
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2018-07-04 21:09:37 -0500

    WiP

---
+++ index.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const fs = require('fs');
+const zlib = require('zlib');
+const {Duplex, Transform} = require('stream');
+
+const {parser} = require('stream-json');
+const {streamValues} = require('stream-json/streamers/StreamValues');
+
+const defaultStreams = ['$-stream-sort-$-0', '$-stream-sort-$-1', '$-stream-sort-$-2'];
+
+const defaultGetReadStream = stream => {
+  return fs
+    .createReadStream(stream)
+    .pipe(zlib.createGunzip())
+    .pipe(parser({streamValues: false}))
+    .pipe(streamValues());
+};
+
+const defaultGetWriteStream = stream => {
+  const t = new Transform({
+    writableObjectMode: true,
+    readableObjectMode: false,
+    transform(chunk, encoding, callback) {
+      this.push(JSON.stringify(chunk));
+      callback(null);
+    }
+  });
+  t.pipe(zlib.createGzip()).pipe(fs.createWriteStream(stream));
+  return t;
+};
+
+class Sort extends Duplex {
+  constructor(options) {
+    super(Object.assign({writableObjectMode: true, readableObjectMode: true}, options));
+    // get options
+    this._less = options.less;
+    this._streams = options.streams || defaultStreams;
+    if (isNaN(this._streams.length) || this._streams.length < 3) {
+      this._streams = defaultStreams;
+    }
+    this._getReadStream = options.getReadStream || defaultGetReadStream;
+    this._getWriteStream = options.getWriteStream || defaultGetWriteStream;
+    this._batchSize = !isNaN(options.batchSize) ? Math.max(1, options.batchSize) : 1024;
+    // set internals
+    this._comp = (a, b) => (this._less(a, b) ? -1 : this._less(b, a) ? 1 : 0);
+    this._batches = this._streams.map((_, index) => ({real: 0, target: index ? 0 : 1}));
+    this._batchStreams = this._batches.map(() => null);
+    this._totalTarget = 1;
+    this._totalReal = 0;
+    this._index = 0;
+    this._batch = [];
+  }
+
+  _write(chunk, encoding, callback) {
+    this._batch.push(chunk);
+    if (this._batch.length >= this._batchSize) {
+      return this._saveBatch(callback);
+    }
+    callback(null);
+  }
+
+  _final(callback) {
+    if (this._batch.length) {
+      return this._saveBatch(callback);
+    }
+    callback(null);
+  }
+
+  _read() {}
+
+  _saveBatch(callback) {
+    this._batch.sort(this._comp);
+
+    if (this._totalReal === this._totalTarget) {
+      // next iteration
+      const target = this._batches[this._index].total;
+      this._batches[this._index].total = 0;
+      this._batches.forEach((_, index) => {
+        if (index !== this._index) {
+          this._batches[index].total += target;
+        }
+      });
+    }
+
+    const data = this._batch;
+    let index = 0;
+
+    this._batch = [];
+  }
+
+  static sort(options) {
+    return new Sort(options);
+  }
+}
+
+module.exports = Sort;
